379a366f10f735e77e29fb190b1d702a5c91c3d7
TahaMajlesi <186780445+TahaMajlesi@users.noreply.github.com>
2025-03-25 03:41:21 -0700

    update

--- Js/LastProjects.js
+++ Js/LastProjects.js
@@ -6,11 +6,11 @@ fetch("../Json/Projects.json")
         for (let i = 0; i < 3; i++) {
             document.getElementById(
                 "lastProjects"
-            ).innerHTML += `<div class="w-4/12 font-mono tracking-wide flex flex-col p-4 hover:bg-white hover:text-black rounded-3xl h-64 gap-y-6 text-gray-300 transition-colors ">
-            <img src="${data[i].icon}" alt="${data[i].alt}">
-            <p class=" font-semibold my-2 w-10/12 text-wrap flex">${data[i].title}</p>
+            ).innerHTML += `<div class="w-4/12 font-mono tracking-wide flex flex-col p-4 hover:bg-white hover:text-black rounded-3xl text-gray-300 transition-colors gap-y-3">
+            <i class="${data[i].icon} text-3xl"></i>
+            <p class=" font-semibold w-10/12 flex text-balance">${data[i].title}</p>
             <p class=" w-10/12 text-wrap flex">${data[i].description}</p>
-            <a href="${data[i].link}" class="text-blue-600 font-semibold my-1">${data[i].link}</a>
+            <a href="${data[i].link}" class="text-blue-600 font-semibold mt-auto">${data[i].title}</a>
             </div>`;
         }
     });
---
+++ Js/links.js
@@ -0,0 +1,5 @@
+$(".goAbout").click(function() {
+    if ($(window).scrollTop() > 100) {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+    }
+});
