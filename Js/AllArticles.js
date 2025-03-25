8f444c67222584d12a61a8305cb1156be3e557dc
TahaMajlesi <186780445+TahaMajlesi@users.noreply.github.com>
2025-03-25 07:41:07 -0700

    Update

---
+++ Js/AllArticles.js
@@ -0,0 +1,14 @@
+fetch("../Json/Articles.json")
+  .then((res) => res.json())
+  .then((data) => {
+    data.reverse();
+    data.forEach((element) => {
+      document.getElementById("AllArticles").innerHTML += `
+            <div class="w-full font-mono tracking-wide flex flex-col"><p class="text-gray-400">| ${element.publishDate}</p>
+            <p class="text-gray-300 font-semibold my-1 w-10/12">${element.title}</p>
+            <p class="text-gray-300 w-10/12">${element.description}</p>
+            <a href="#!" class="text-blue-600 font-semibold my-1">Read article ></a>
+            </div>
+`;
+    });
+  });
