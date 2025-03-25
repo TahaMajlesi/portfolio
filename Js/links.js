88613596c34994b8404d2e89381897e694e43e4c
TahaMajlesi <186780445+TahaMajlesi@users.noreply.github.com>
2025-03-25 07:47:18 -0700

    update

--- Js/links.js
+++ Js/links.js
@@ -1,4 +1,4 @@
-$(".goAbout , .goProjects").click(function () {
+$(".goAbout , .goProjects , .goArticles").click(function () {
   if ($(window).scrollTop() > 100) {
     $("html, body").animate({ scrollTop: 0 }, "slow");
   }
