2dfe87076465ed3fa12d7b700da7f62ead7156e0
TahaMajlesi <186780445+TahaMajlesi@users.noreply.github.com>
2025-03-26 01:00:56 -0700

    update

--- Js/Nav.js
+++ Js/Nav.js
@@ -1,10 +1,10 @@
 let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-    let currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navBar").style.top = "16px";
-    } else {
-        document.getElementById("navBar").style.top = "-50px";
-    }
-    prevScrollpos = currentScrollPos;
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navBar").style.top = "16px";
+  } else {
+    document.getElementById("navBar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
 };
